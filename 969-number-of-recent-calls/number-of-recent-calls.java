35565c0a8b5da662208719c9227f5d5a0843e245
RakeshKonkimalla <rakesh3588hs@gmail.com>
2026-08-24 10:25:07 +0530

    Time: 17 ms (99.85%) | Memory: 52.7 MB (81.11%) - LeetSync

--- 969-number-of-recent-calls/number-of-recent-calls.java
+++ 969-number-of-recent-calls/number-of-recent-calls.java
@@ -2,12 +2,10 @@ class RecentCounter {
     private static final int[] records = new int[10000]; //
     private int start;
     private int end;
-
     public RecentCounter() {        
         start = 0;
         end = 0;
     }
-    
     public int ping(int t) {
         while (start < end && (t - records[start] > 3000)) { 
             start++;
